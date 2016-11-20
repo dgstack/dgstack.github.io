52fd4b2baf5038adb548fdc78b169609c959168f
Digvijay Bhakuni <digvijay.bhakuni@rediffmail.com>
2016-11-20 14:32:49 +0530

    Image Align, 404, Nav Shade

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,6 +7,13 @@
 		});  	
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
 	}); 
+    
+    $(".col-lg-9 p>img").each(function(e){
 
+        var caption = "<br/><span>"+this.getAttribute("alt")+"</span>";
+        $(this).parent().addClass("p-caption-container").append(caption);
+
+    });
+    
 		
 })(jQuery);
